5064dc08c5a928b74034910c7ea528de081758cd
gustasousagh <158321110+gustasousagh@users.noreply.github.com>
2024-05-14 16:13:32 -0300

    ilegra logo add

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -4,6 +4,7 @@ import { ThemeToggle } from "./ThemeTogle";
 import { MetricsButton } from "./metrics-button";
 import { SearchInput } from "./search-input";
 import { Suspense } from "react";
+import { Logo } from "./Logo";
 
 
 
@@ -12,7 +13,7 @@ export const Navbar = ({
   return (
     <div className="border-b">
       <div className="flex h-14 items-center">
-        Logo
+        <Logo />
         <div className="ml-auto flex items-center space-x-2">
           <Suspense fallback={<div></div>}>
             <SearchInput />
