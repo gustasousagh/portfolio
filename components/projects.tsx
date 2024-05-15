092d39bba5c901fc15691a33ddce486875f6ed86
gustasousagh <158321110+gustasousagh@users.noreply.github.com>
2024-05-15 14:57:29 -0300

    error list key fix

--- components/projects.tsx
+++ components/projects.tsx
@@ -10,9 +10,10 @@ export const Projects = ({
 }: PrjectsListProps) => {
   return (
     <div>
-      <div className="grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-4">
+      <div className="z-50 grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-4">
         {items.map((item) => (
-          <ProjectCard 
+          <ProjectCard
+          key={item.id}
           id={item.id}
           title={item.title}
           description={item.description}
